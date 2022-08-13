4562a0941531a87073cceaf399119df2c1f71393
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2022-08-14 03:39:26 +0900

    Setup: 절대경로 설정

--- babel.config.js
+++ babel.config.js
@@ -5,11 +5,29 @@ module.exports = {
   },
   plugins: [
     [
+      "module-resolver",
+      {
+        root: ["."],
+        alias: {
+          "#components": "./app/components",
+          "#navigators": "./app/navigators",
+          "#screens": "./app/screens",
+          "#theme": "./app/theme",
+          "#api": "./app/services/api",
+          "#images": "./assets/images",
+          "#fonts": "./assets/fonts",
+          "#storybook": "./storybook",
+          ".": ["./*"],
+        },
+      },
+    ],
+    [
       "@babel/plugin-proposal-decorators",
       {
         legacy: true,
       },
     ],
     ["@babel/plugin-proposal-optional-catch-binding"],
+    
   ],
 }
